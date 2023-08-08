31c33eca03063db945f92eed471e717b728b13d3
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2023-08-09 06:45:23 +0800

    numberFormatter completed

--- src/components/NumberFormatter/index.tsx
+++ src/components/NumberFormatter/index.tsx
@@ -4,25 +4,63 @@ import styles from "./index.module.scss";
 export default function NumberFormatter() {
   const [inputNumber, setInputNumber] = useState<string>("-7855948.9527");
 
-  function ThousandSeparator(number: string): string {
+  const ThousandSeparator = (number: string): string => {
     const parts = number.split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
-  }
+  };
 
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    
-    const inputValue = event.target.value;
-    const validInput = inputValue.replace(/[^\d.-]/g, "");
-    setInputNumber(validInput);
-  }
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let inputValue = event.target.value;
+    inputValue = inputValue.replace(/[^\d.-]/g, "");
+
+    inputValue = inputValue.replace(/^\./g, "");
+    inputValue = inputValue.replace(/\.{2,}/g, ".");
+    inputValue = inputValue
+      .replace(".", "$#$")
+      .replace(/\./g, "")
+      .replace("$#$", ".");
+
+    if (inputValue.indexOf("-") > 0) {
+      inputValue = inputValue.replace("-", "");
+    } else if (inputValue.indexOf("-") === 0) {
+      inputValue = "-" + inputValue.replace(/-/g, "");
+    }
+
+    if (inputValue.startsWith("-") && inputValue.charAt(1) === ".") {
+      inputValue = "-" + inputValue.slice(1).replace(".", "");
+    }
+
+    if (inputValue.startsWith("0") && !inputValue.startsWith("0.")) {
+      if (inputValue.length > 1) {
+        inputValue = "0." + inputValue.slice(1);
+      } else {
+        inputValue = "0";
+      }
+    }
+
+    if (inputValue.startsWith("-0") && !inputValue.startsWith("-0.")) {
+      if (inputValue.length > 2) {
+        inputValue = "-0." + inputValue.slice(2);
+      } else if (inputValue.length > 1) {
+        inputValue = "-0";
+      } else {
+        inputValue = "-";
+      }
+    }
+
+    setInputNumber(inputValue);
+  };
 
   const formattedNumber: string = ThousandSeparator(inputNumber);
 
   return (
     <div className={styles.numberFormatter}>
       <div className={styles.numberFormatter__input}>
-        <label className={styles.numberFormatter__inputLabel} htmlFor="inputNumber">
+        <label
+          className={styles.numberFormatter__inputLabel}
+          htmlFor="inputNumber"
+        >
           input:
         </label>
         <input
@@ -36,7 +74,9 @@ export default function NumberFormatter() {
       </div>
       <p className={styles.numberFormatter__output}>
         <span className={styles.numberFormatter__outputLabel}>output:</span>
-        <span className={styles.numberFormatter__outputValue}>{formattedNumber}</span>
+        <span className={styles.numberFormatter__outputValue}>
+          {formattedNumber}
+        </span>
       </p>
     </div>
   );
